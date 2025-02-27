cc5b27c09f609bf3797b4cf0cb6dc57fd52349de
bagomed <bogdan.efimov.05@mail.ru>
2025-02-27 22:25:35 +0300

    pochti pochti carouse

--- src/Components/Layout/TVCarousel.tsx
+++ src/Components/Layout/TVCarousel.tsx
@@ -11,12 +11,17 @@ export function TVCarousel() {
     const ELEMENTS_COUNT = 10;
     const BLOCK_WIDTH = ELEMENT_WIDTH + CAROUSEL_MARGIN_LEFT;
     const CAROUSEL_WIDTH = BLOCK_WIDTH * ELEMENTS_COUNT;
+    const MID_INDEX = Math.floor((ELEMENTS_COUNT - 1) / 2);
     // px 
-    const [choosenElement, setChoosenElement] = useState(4);
+    const [choosenElement, setChoosenElement] = useState(MID_INDEX);
     const [offset, setOffset] = useState(
-        -BLOCK_WIDTH * Math.floor((ELEMENTS_COUNT - 1) / 2)
+        -BLOCK_WIDTH * MID_INDEX
         // STARTING OFFSET
     );
+
+    // const [indexRight, setIndexLeft] = useState(0);
+    // const [indexLeft, setIndexRight] = useState(0);
+    // HZ
 
     // DECLARING USEREFS
     const carousel = useRef<HTMLDivElement | null>(null);
@@ -28,8 +33,17 @@ export function TVCarousel() {
     // 
     const filters = useRef<Array<HTMLDivElement>>([]);
     const pushFilters = (el: HTMLDivElement) => filters.current.push(el);
-
     const filtersStyles = useRef<Array<CSSStyleDeclaration>>([]);
+    //
+    const buttonLeft = useRef<HTMLButtonElement | null>(null);
+    const buttonLeftStyles = useRef<CSSStyleDeclaration | null>(null);
+    const buttonRight = useRef<HTMLButtonElement | null>(null);
+    const buttonRightStyles = useRef<CSSStyleDeclaration | null>(null);
+    //
+    const switchers = useRef<Array<HTMLButtonElement>>([]);
+    const pushSwitchers = (el: HTMLButtonElement) => switchers.current.push(el);
+    const switchersStyles = useRef<Array<CSSStyleDeclaration>>([]);
+    //
     const elementsOffsets = useRef<Array<number>>(new Array(ELEMENTS_COUNT).fill(0));
     // ELEMENTS OFFSETS, INITITALLY ARRAY OF 0
     const indexLL = useRef<number>(0);
@@ -38,12 +52,14 @@ export function TVCarousel() {
     const indexL = useRef<number>(0);
     const indexR = useRef<number>(ELEMENTS_COUNT);
     // THE LEAST LEFT AND RIGHT INDEXES
-
     const moveDir = useRef<String>('n')
     //  MOVE DIRECTION, 'r' IS RIGHT, 'l' IS LEFT
 
+
+
+
     function calcIndexLL() {
-        let _indexLL = choosenElement - Math.floor((ELEMENTS_COUNT - 1) / 2)
+        let _indexLL = choosenElement - MID_INDEX;
         if (_indexLL > 0) {
             _indexLL = _indexLL % ELEMENTS_COUNT
         }
@@ -68,6 +84,8 @@ export function TVCarousel() {
         // -indexLL + ELEMENTS_COUNT * X
         indexRR.current = _indexRR;
     }
+    // HZ
+
     function calcIndexL() {
         let _indexLL = choosenElement - 1
         if (_indexLL > 0) {
@@ -76,9 +94,6 @@ export function TVCarousel() {
         else {
             _indexLL = (_indexLL + ELEMENTS_COUNT) % ELEMENTS_COUNT
         }
-        // choosenElement - 4 - ELEMENTS_COUNT || choosenElement - 4 || choosenElement - 4 + ELEMENTS_COUNT
-        // indexLL - ELEMENTS_COUNT * X
-        // -indexLL + ELEMENTS_COUNT * X
         indexL.current = _indexLL;
     }
     function calcIndexR() {
@@ -94,13 +109,14 @@ export function TVCarousel() {
         // -indexLL + ELEMENTS_COUNT * X
         indexR.current = _indexRR;
     }
-    // THE MOST LEFT AND THE MOST RIGHT INDEXES
 
-    let id = -2;
+
+    let id = -1;
     let json_info = JSON_RECIEVED_INFO.map(function (imageUrl) {
         id += 1;
         return ([id, imageUrl.imageUrl, "carouselElements", "filters"])
     })
+    // JSON READING 
 
 
     useEffect(() => {
@@ -129,96 +145,178 @@ export function TVCarousel() {
                 }
             }
         }
+        if (buttonLeft.current && buttonRight.current) {
+            buttonLeftStyles.current = buttonLeft.current!.style;
+            buttonRightStyles.current = buttonRight.current!.style;
+        }
+        else {
+            console.log("BUTTONS IS NULL");
+        }
+        if (switchers.current) {
+            for (let i = 0; i < ELEMENTS_COUNT; i++) {
+                if (switchers.current![i]) {
+                    switchersStyles.current![i] = (switchers.current![i] as HTMLElement).style
+                }
+                else {
+                    console.log("Switchers[" + i + "] is null!");
+                }
+            }
+
+        }
+
         // ASSIGNING  USEREFS
         for (let i = 0; i < ELEMENTS_COUNT; i++) {
             if (i != choosenElement + 1) {
                 filtersStyles.current![i].filter = "opacity(70%)";
+
+                switchersStyles.current![i].background = "var(--gray-c1)";
+                // FIX
             }
             else {
                 filtersStyles.current![i].filter = "opacity(0%)"
+                switchersStyles.current![i].background = "var(--gray-c2)";
+                // FIX
             }
             carouselElementsStyles.current![i].minWidth = ELEMENT_WIDTH + "px";
             carouselElementsStyles.current![i].maxWidth = ELEMENT_WIDTH + "px";
+
+
         }
 
-        carouselStyles.current!.transition = "margin-left 1s";
+        buttonLeftStyles.current!.width = BLOCK_WIDTH + "px";
+        buttonLeftStyles.current!.marginLeft = -2 * BLOCK_WIDTH + "px";
 
+        buttonRightStyles.current!.width = BLOCK_WIDTH + "px";
+        buttonRightStyles.current!.marginLeft = 2 * BLOCK_WIDTH + "px";
+
+        carouselStyles.current!.transition = "margin-left 1s";
         // ASSIGNEMENT CAROUSELELEMENTS WIDTH AND FINLTERS OPACITY AND TRANSITION
 
 
     }, [])
+    // STARTING VALUES SETTING
     useEffect(() => {
         carouselStyles.current!.marginLeft = offset + "px";
     }, [offset])
     // MOVING CAROUSEL SCRIPT
 
     useEffect(() => {
-
-        // if (0 < choosenElement && choosenElement < ELEMENTS_COUNT) {
-        //     let filterStyles = (filters.current![(choosenElement + 1)] as HTMLElement).style;
-        //     filterStyles.filter = "opacity(70%)";
-        //     filterStyles = (filters.current![(choosenElement - 1)] as HTMLElement).style;
-        //     filterStyles.filter = "opacity(70%)";
-        //     filterStyles = (filters.current![(choosenElement)] as HTMLElement).style;
-        //     filterStyles.filter = "opacity(0%)";
-        // }    
+        console.log("useeffect");
         calcIndexR();
         calcIndexL();
 
         filtersStyles.current![indexL.current].filter = "opacity(70%)";
-        // console.log(indexL.current, 'IL');
-        filtersStyles.current![indexR.current].filter = "opacity(70%)";
-        // console.log(indexR.current, 'IR');
         filtersStyles.current![choosenElement].filter = "opacity(0%)";
-        // console.log(choosenElement, "CE");
+        filtersStyles.current![indexR.current].filter = "opacity(70%)";
+
+        switchersStyles.current![indexL.current].backgroundColor = "var(--gray-c1)";
+        switchersStyles.current![choosenElement].backgroundColor = "var(--gray-c2)";
+        switchersStyles.current![indexR.current].backgroundColor = "var(--gray-c1)";
+
+
 
 
         if (moveDir.current == 'r') {
             elementsOffsets.current[indexLL.current] += CAROUSEL_WIDTH;
-            // console.log(elementsOffsets.current[indexLL.current]);
             carouselElementsStyles.current![indexLL.current].transform = 'translate(' + elementsOffsets.current[indexLL.current] + 'px,0px)';
         }
         if (moveDir.current == 'l') {
             elementsOffsets.current[indexRR.current] -= CAROUSEL_WIDTH;
             carouselElementsStyles.current![indexRR.current].transform = 'translate(' + elementsOffsets.current[indexRR.current] + 'px,0px)';
-            // console.log(elementsOffsets.current[indexLL.current]);
         }
+
+
+
+        // if (moveDir.current == 'r') {
+        //     elementsOffsets.current[indexLeft] += CAROUSEL_WIDTH;
+        //     carouselElementsStyles.current![indexLeft].transform = 'translate(' + elementsOffsets.current[indexLeft] + 'px,0px)';
+        // }
+        // if (moveDir.current == 'l') {
+        //     elementsOffsets.current[indexRight] -= CAROUSEL_WIDTH;
+        //     carouselElementsStyles.current![indexRight].transform = 'translate(' + elementsOffsets.current[indexRight] + 'px,0px)';
+        // }
+        // HZ
+
 
     }, [choosenElement])
     // CHANGING FILTERS OPACITY WHEN MOVING
 
     function handleLeftArrowClick() {
-        setOffset(offset + BLOCK_WIDTH);
+        // setOffset(offset + BLOCK_WIDTH);
+        setOffset(prevOffset => prevOffset + BLOCK_WIDTH);
+
         calcIndexRR();
 
         moveDir.current = 'l';
 
         if (choosenElement - 1 >= 0) {
-            setChoosenElement(choosenElement - 1);
+            // setChoosenElement(choosenElement - 1);
+            setChoosenElement(prevChoosenElement => prevChoosenElement - 1);
         }
         else {
-            setChoosenElement(choosenElement - 1 + ELEMENTS_COUNT);
+            setChoosenElement(prevChoosenElement => prevChoosenElement - 1 + ELEMENTS_COUNT);
         }
 
     }
     function handleRightArrowClick() {
-        setOffset(offset - BLOCK_WIDTH);
+        console.log('rightarrowclock');
+        // setOffset(offset - BLOCK_WIDTH);
+        setOffset(prevOffset => prevOffset - BLOCK_WIDTH);
+
         calcIndexLL();
+
 
         moveDir.current = 'r';
 
         if (choosenElement + 1 < ELEMENTS_COUNT) {
-            setChoosenElement(choosenElement + 1);
+            setChoosenElement(prevChoosenElement => prevChoosenElement + 1);
         }
         else {
-            setChoosenElement(choosenElement + 1 - ELEMENTS_COUNT);
+            setChoosenElement(prevChoosenElement => prevChoosenElement + 1 - ELEMENTS_COUNT);
+        }
+    }
+    function switchHandler(event: React.MouseEvent<HTMLButtonElement>) {
+        let switcherNumber = Number(event.currentTarget.dataset.number);
+        if (!switcherNumber) { console.log('SWITCHER NUMBER IS NULL') }
+
+        if (choosenElement < switcherNumber!) {
+            let steps = switcherNumber - choosenElement;
+            if (steps > 0) {
+                let i = 0;
+                const interval = setInterval(() => {
+                    // setOffset(prevOffset => prevOffset - BLOCK_WIDTH);
+                    handleRightArrowClick();
+                    i++;
+                    if (i >= steps) clearInterval(interval);
+                }, 0); // 0 ms, но React всё равно выполнит обновления последовательно
+            }
+
+        }
+        if (choosenElement > switcherNumber) {
+            let steps = choosenElement - switcherNumber;
+            if (steps > 0) {
+                let i = 0;
+                const interval = setInterval(() => {
+                    // setOffset(prevOffset => prevOffset - BLOCK_WIDTH);
+                    handleLeftArrowClick();
+                    i++;
+                    if (i >= steps) clearInterval(interval);
+                }, 0); // 0 ms, но React всё равно выполнит обновления последовательно
+            }
+
         }
     }
 
+
+
     return (
         <div className="tv-carousel">
+
             <div className="tv-carousel-container">
+                <button className="btn btn-left" ref={buttonLeft} onClick={handleLeftArrowClick}> LL </button>
+                <button className="btn btn-right" ref={buttonRight} onClick={handleRightArrowClick}> RR </button>
                 <div className="tv-carousel-window">
+
                     <div className="tv-carousel-row" ref={carousel}>
 
                         {/* Можно ли использовать htmlELement для htmlDivElement??*/}
@@ -239,9 +337,21 @@ export function TVCarousel() {
                     </div>
                 </div>
             </div>
-            <button onClick={handleLeftArrowClick}> left </button>
-            <button onClick={handleRightArrowClick}> right </button>
-
-        </div>
+            <div className="switcher-row">
+                {json_info.map(function (elementInfo) {
+                    let id = elementInfo[0] as number;
+                    return (
+                        <button
+                            key={id}
+                            data-number={id}
+                            id={"tv-carousel-switcher-" + id}
+                            onClick={switchHandler}
+                            ref={pushSwitchers}
+                            className="switcher" >
+                        </button>
+                    );
+                })}
+            </div>
+        </div >
     );
 }
